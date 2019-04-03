2f50baa5ec52a19610efdf05945a5c3279bdd465
guillermo377 <guillermo377@gmail.com>
2019-04-03 17:07:48 +0200

    Tookit Updated

--- powershell.ps1
+++ powershell.ps1
@@ -8,11 +8,20 @@ Param(
 #Variables
 $toolkitpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\Toolkit.zip"
 
-
-
 #Initialize Data Disks
-Get-Disk | ` 
+<#Get-Disk | ` 
 Where partitionstyle -eq 'raw' | ` 
 Initialize-Disk -PartitionStyle MBR -PassThru | ` 
 New-Partition -AssignDriveLetter -UseMaximumSize | ` 
-Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false#>
+
+##Extracting Toolkit
+$DestinationPath = "C:\Temp"
+Test-Path $DestinationPath
+if ($DestinationPath){
+Expand-Archive -Path $toolkitpath -DestinationPath $DestinationPath -Force -Confirm 
+}else{
+New-Item -ItemType directory -Path C:\Temp´Expand-Archive -Path $toolkitpath -DestinationPath $DestinationPath -Force -Confirm 
+}
+##Launch Installation
+powershell.exe -ExecutionPolicy Unrestricted -File ($DestinationPath +"\Deploy-Application.ps1")
