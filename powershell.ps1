0ac4719e8fefa84b021bf4364d28792022873d6c
guillermo377 <guillermo377@gmail.com>
2019-04-03 16:12:06 +0200

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,1 +1,18 @@
+[CmdletBinding()]
 
+Param(
+    [string]$USERNAME,
+    [string]$PASSWORD
+)
+
+#Variables
+$toolkitpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\Toolkit.zip"
+
+
+
+#Initialize Data Disks
+Get-Disk | ` 
+Where partitionstyle -eq 'raw' | ` 
+Initialize-Disk -PartitionStyle MBR -PassThru | ` 
+New-Partition -AssignDriveLetter -UseMaximumSize | ` 
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
